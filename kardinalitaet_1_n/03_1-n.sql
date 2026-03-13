822f32318b26b95bf19cae8eac5266b332896e44
yWarsame <yasinhassan1013@gmail.com>
2026-03-13 10:52:17 +0100

    03_Kardinalitaet N-M Relations  docs: Add sleep delays for better script visualization

--- kardinalitaet_1_n/03_1-n.sql
+++ kardinalitaet_1_n/03_1-n.sql
@@ -1,28 +1,58 @@
+\! cls
 
--- Vorbereitung
+# Vorbereitung
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS design.kittens;
+DROP TABLE IF EXISTS design.catmoms;
+
+# MT: cats
+CREATE TABLE IF NOT EXISTS design.catmoms
+(
+  id INT NOT NULL AUTO_INCREMENT,
+  cat_name  VARCHAR(45) NOT NULL,
+  fur_color VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+# MT: Struktur
+DESCRIBE design.catmoms;
+
+# MT: Inserts
+INSERT INTO design.catmoms (id, cat_name, fur_color) VALUES 
+(DEFAULT, "Grizabella", "white"),
+(DEFAULT, "Mausi", "striped");
+
+# MT: Inhalte
+SELECT * FROM design.catmoms;
+
+# DT: kittens
+CREATE TABLE IF NOT EXISTS design.kittens
+(
+  id INT NOT NULL AUTO_INCREMENT,
+  kitten_name VARCHAR(45) NOT NULL,
+  fur_color VARCHAR(45) NOT NULL,
+  catmoms_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+ALTER TABLE design.kittens
+  ADD CONSTRAINT FK_catmoms_TO_kittens
+    FOREIGN KEY (catmoms_id)
+    REFERENCES catmoms (id);
+
+# DT: Struktur
+DESCRIBE design.kittens;
+
+# DT: Inserts
+INSERT INTO design.kittens (id, kitten_name, fur_color, catmoms_id) VALUES 
+(DEFAULT, "Grizzi_1", "white", 1),
+(DEFAULT, "Grizzi_2", "white", 1),
+(DEFAULT, "Mausini", "striped", 2);
+
+# DT: Inhalte
+SELECT * FROM design.kittens;
 
 
-
--- MT: catmoms
-
-
--- MT: Struktur
-
-
--- MT: Inserts
-
-
--- MT: Inhalte
-
-
--- DT: kittens
-
-
--- DT: Struktur
-
-
--- DT: Inserts
-
-
--- DT: Inhalte
 
--- kardinalitaet_n_m_I/03_n-m.sql
+++ kardinalitaet_n_m_I/03_n-m.sql
@@ -1,54 +1,106 @@
+\! cls
 
--- Vorbereitung
+# Vorbereitung
 
+SET NAMES utf8mb4;
 
+DROP TABLE IF EXISTS design.purchases;
+DROP TABLE IF EXISTS design.products;
+DROP TABLE IF EXISTS design.servants;
 
 /* SERVANTS */
-
-
 -- Servants: ohne Fremdschlüssel
-
+CREATE TABLE design.servants
+(
+  id INT NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  servant_name VARCHAR(45) NOT NULL,
+  yrs_served TINYINT NOT NULL,
+  PRIMARY KEY (id)
+) COMMENT 'Tab. der Diener';
 
 -- Servants: Struktur
-
+DESCRIBE design.servants;
 
 -- Servants: Inserts
-
-
+# Servants: Inserts
+INSERT INTO design.servants (id, servant_name, yrs_served) VALUES (DEFAULT, "Max", 5);
+INSERT INTO design.servants (id, servant_name, yrs_served) VALUES (DEFAULT, "Maxine", 3);
 
 -- Servants: Inhalte 
+SELECT * FROM design.servants;
 
-
+-- Time delay for demonstration purposes
+DO SLEEP(1);
 
 /*  PRODUCTS */
 
 -- Products: ohne Fremdschlüssel
-
-
-
+CREATE TABLE design.products
+(
+  id INT NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  product_name  VARCHAR(45)  NOT NULL,
+  product_price DECIMAL(4,2) NOT NULL,
+  PRIMARY KEY (id)
+) COMMENT 'Tab. der Produkte';
 
 -- Products: Struktur
-
+DESCRIBE design.products;
 
 -- Products: Inserts
-
-
+INSERT INTO design.products (id, product_name, product_price) VALUES 
+(DEFAULT, "Whiskas|Lachs", 2.75),
+(DEFAULT, "Whiskas|Huhn", 2.80),
+(DEFAULT, "Felix|Jelly", 3.75),
+(DEFAULT, "Felix|Sauce", 3.80);
 
 -- Products: Inhalte 
+SELECT * FROM design.products;
 
+-- Time delay for demonstration purposes
+DO SLEEP(1);
 
 /*  PURCHASES (Kaufprozesse)*/
-
 -- ServantsProducts (purchases)
+CREATE TABLE design.purchases
+(
+  id INT NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  servants_id INT NOT NULL COMMENT 'FK von servants',
+  products_id INT NOT NULL COMMENT 'FK  von products',
+  p_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Zeitpunkt',
+  PRIMARY KEY (id)
+) COMMENT 'Kaufhandlungen';
 
+-- fks
+ALTER TABLE design.purchases
+  ADD CONSTRAINT FK_servants_TO_purchases
+    FOREIGN KEY (servants_id)
+    REFERENCES design.servants (id);
 
+ALTER TABLE design.purchases
+  ADD CONSTRAINT FK_products_TO_purchases
+    FOREIGN KEY (products_id)
+    REFERENCES design.products (id);
 
 -- Purchases: Struktur
-
+DESCRIBE design.purchases;
 
 -- Purchases: Inserts (Kaufprozesse : Käufer - Produkt)
+INSERT INTO design.purchases (id, servants_id, products_id, p_time) VALUES (DEFAULT, 1 , 2, DEFAULT);
+DO SLEEP(1);
 
+INSERT INTO design.purchases (id, servants_id, products_id, p_time) VALUES (DEFAULT, 1, 3, DEFAULT);
+DO SLEEP(1);
 
+INSERT INTO design.purchases (id, servants_id, products_id, p_time) VALUES (DEFAULT, 2, 1, DEFAULT);
+DO SLEEP(1);
 
+INSERT INTO design.purchases (id, servants_id, products_id, p_time) VALUES (DEFAULT, 2, 2, DEFAULT);
+DO SLEEP(1);
+
+INSERT INTO design.purchases (id, servants_id, products_id, p_time) VALUES (DEFAULT, 2, 3, DEFAULT);
+DO SLEEP(1);
+
+INSERT INTO design.purchases (id, servants_id, products_id, p_time) VALUES (DEFAULT, 2, 4, DEFAULT);
 
 -- Purchases: Inhalte 
+SELECT * FROM design.purchases;
