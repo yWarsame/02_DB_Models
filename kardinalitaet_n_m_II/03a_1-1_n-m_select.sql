40e57d0a03794d56c3b0816c06b34c0977f025f5
yWarsame <yasinhassan1013@gmail.com>
2026-03-16 12:37:46 +0100

    03b / 1-1 & n-m VIEW II

--- kardinalitaet_n_m_II/03a_1-1_n-m_select.sql
+++ kardinalitaet_n_m_II/03a_1-1_n-m_select.sql
@@ -1,25 +1,77 @@
-
--- 3b SELECTS 1:1 / n:m
+\! cls
+-- 3a SELECTS 1:1 / n:m
 
 -- 1. JOIN 
-
-
-
-
+-- SELECT
+-- 	*
+-- FROM design.purchases
+-- INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+-- INNER JOIN design.products ON design.products.id = design.purchases.products_id
+-- INNER JOIN design.cats ON design.cats.id = design.servants.cats_id
+-- ;
 
 -- 2. Wer bekommt den Lachs? 
-
-
-
+# 2. Wer bekommt den Lachs? 
+-- SELECT
+-- 	cat_name AS Herrschaft,
+--     servant_name AS Diener,
+--     product_name AS Produkt
+-- FROM design.purchases
+-- INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+-- INNER JOIN design.products ON design.products.id = design.purchases.products_id
+-- INNER JOIN design.cats ON design.cats.id = design.servants.cats_id
+-- WHERE product_name LIKE "%Lachs%" -- irgendwas mit Lachs...
+-- ;
 
 -- 2a. Wer bekommt den Lachs? 
+-- Version mit REGEXP, damit auch "Lachsfilet" gefunden wird:
+SELECT
+    cat_name AS Herrschaft,
+    servant_name AS Diener,
+    product_name AS Produkt
+FROM design.purchases
+INNER JOIN design.servants ON servants.id = purchases.servants_id
+INNER JOIN design.products ON products.id = purchases.products_id
+INNER JOIN design.cats ON cats.id = servants.cats_id
+WHERE product_name REGEXP 'Lachs';
 
-
-
-
+ # 2a. Wer bekommt den Lachs? 
+-- SELECT
+-- 	concat(
+-- 			servant_name, 
+--             " ist der Diener von ", 
+--             cat_name,", er kauft ",
+--             product_name, 
+--             "- somit bekommt ",
+--             cat_name, " den Lachs."
+--             ) 
+--             AS "Wer bekommt den Lachs?"
+-- FROM design.purchases
+-- INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+-- INNER JOIN design.products ON design.products.id = design.purchases.products_id
+-- INNER JOIN design.cats ON design.cats.id = design.servants.cats_id
+-- WHERE product_name LIKE "%Lachs%" -- irgendwas mit Lachs...
+-- ;
 
 -- 2b. Variante mit einer VIEW
+# 2b. Variante mit einer VIEW
+DROP VIEW IF EXISTS design.who_purchased_salmon;
+CREATE VIEW  design.who_purchased_salmon AS
+	SELECT
+		servant_name AS Diener,
+		cats_id
+	FROM design.purchases
+	INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+	INNER JOIN design.products ON design.products.id = design.purchases.products_id
+	WHERE product_name LIKE "%Lachs%" -- irgendwas mit Lachs...
+;
 
+SELECT * FROM design.who_purchased_salmon;
+
+SELECT
+	 concat(cat_name, " bekommt den Lachs.") AS Gourmet
+FROM design.cats INNER JOIN design.who_purchased_salmon  --  VIEW wird als Tabelle behandelt
+ON design.cats.id = design.who_purchased_salmon.cats_id;
 
 
 
--- kardinalitaet_n_m_II/03b_1-1_n-m_select.sql
+++ kardinalitaet_n_m_II/03b_1-1_n-m_select.sql
@@ -1,24 +1,62 @@
 -- 3b SELECTS 1:1 / n:m
 
 -- 1. JOIN 
-
+-- SELECT
+--  	*
+-- FROM design.purchases
+-- INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+-- INNER JOIN design.products ON design.products.id = design.purchases.products_id
+-- INNER JOIN design.cats ON design.cats.id = design.servants.cats_id
+-- ;
 
 
 
 -- 2. JOIN mit allen Inhalts-Feldern als VIEW
-
+-- - DROP VIEW IF EXISTS content_view;
+-- CREATE OR REPLACE VIEW content_view AS
+-- SELECT
+-- 	servant_name, 
+--     yrs_served,
+-- 	product_name,
+--     product_price,
+-- 	cat_name, 
+--     fur_color
+-- FROM design.purchases
+-- INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+-- INNER JOIN design.products ON design.products.id = design.purchases.products_id
+-- INNER JOIN design.cats ON design.cats.id = design.servants.cats_id
+-- ;
+-- SELECT * FROM content_view;
 
 -- 2. Wer bekommt den Lachs? 
-
-
-
-
--- 2a. Wer bekommt den Lachs? 
-
-
-
-
+-- SELECT
+-- 	cat_name AS Herrschaft,
+--     servant_name AS Diener,
+--     product_name AS Produkt
+-- FROM content_view -- eine VIEW wird wie eine Tabelle behandelt
+-- WHERE product_name LIKE "%Lachs%" -- irgendwas mit Lachs...
+-- ;
 
 -- 2a. Wer bekommt den Lachs? 
+
+-- SELECT
+-- 	concat(
+-- 			servant_name, 
+--             " ist der Diener von ", 
+--             cat_name,", er kauft ",
+--             product_name, 
+--             "- somit bekommt ",
+--             cat_name, " den Lachs."
+--             ) 
+--             AS "Wer bekommt den Lachs?"
+-- FROM content_view
+-- WHERE product_name LIKE "%Lachs%" -- irgendwas mit Lachs...
+-- ;
+
+-- 2b. Wer bekommt den Lachs? 
+SELECT
+	 concat(cat_name, " bekommt den Lachs.") AS Gourmet
+FROM content_view
+WHERE product_name LIKE "%Lachs%" -- irgendwas mit Lachs...
 
 
